8f4b86dd2d288d822e7dc61caf99189d2304bc44
ze-antunes <jprantunes2000@gmail.com>
2023-12-17 18:13:02 +0000

    update files

---
+++ src/components/Candies.js
@@ -0,0 +1,59 @@
+import React, { useRef, useLayoutEffect } from "react";
+import { useGLTF, useScroll } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import gsap from "gsap";
+
+export function Candies(props) {
+  // const { nodes, materials } = useGLTF('/candies-transformed.glb')
+
+  // const candies = useRef();
+  // const scroll = useScroll();
+  // const tl = useRef();
+
+  // useFrame((state, delta) => {
+  //   tl.current.seek(scroll.offset * tl.current.duration());
+  // });
+
+  // useLayoutEffect(() => {
+  //   tl.current = gsap.timeline({
+  //     defaults: { duration: 2, ease: "power1.inOut" }
+  //   });
+
+  //   tl.current
+  //     .to(candies.current.rotation, { y: -1 }, 2)
+  //     .to(candies.current.position, { x: 1 }, 2)
+
+  //     .to(candies.current.rotation, { y: 1 }, 6)
+  //     .to(candies.current.position, { x: -1 }, 6)
+
+  //     .to(candies.current.rotation, { y: 0 }, 11)
+  //     .to(candies.current.rotation, { x: 1 }, 11)
+  //     .to(candies.current.position, { x: 0 }, 11)
+
+  //     .to(candies.current.rotation, { y: 0 }, 13)
+  //     .to(candies.current.rotation, { x: -1 }, 13)
+  //     .to(candies.current.position, { x: 0 }, 13)
+
+  //     .to(candies.current.rotation, { y: 0 }, 16)
+  //     .to(candies.current.rotation, { x: 0 }, 16)
+  //     .to(candies.current.position, { x: 0 }, 16)
+
+  //     .to(candies.current.rotation, { y: 0 }, 20)
+  //     .to(candies.current.rotation, { x: 0 }, 20)
+  //     .to(candies.current.position, { x: 0 }, 20);
+  // }, []);
+  return (
+    <group {...props} dispose={null} >
+      <mesh>
+        <boxGeometry></boxGeometry>
+        <meshBasicMaterial></meshBasicMaterial>
+      </mesh>
+      {/* <mesh geometry={nodes.toffee__golden001.geometry} material={materials['toffee _golden.003']} position={[0.01, 0.03, -0.04]} rotation={[-0.69, -0.27, 0.67]} scale={-1} />
+      <mesh geometry={nodes.toffee__golden002.geometry} material={materials['toffee _golden.003']} position={[0, 0, 0.03]} rotation={[0.13, -0.08, 1.7]} scale={-1} />
+      <mesh geometry={nodes.toffee__golden003.geometry} material={materials['toffee _golden.003']} position={[0.05, 0.02, 0]} rotation={[-0.66, 0.53, 1.59]} scale={-1} />
+      <mesh geometry={nodes.toffee__golden.geometry} material={materials['toffee _golden.003']} position={[-0.06, 0.01, -0.02]} rotation={[-0.26, 0, 1.17]} scale={-1} /> */}
+    </group>
+  )
+}
+
+useGLTF.preload('/candies-transformed.glb')
--- src/pages/Historia.js
+++ src/pages/Historia.js
@@ -1,12 +1,59 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useRef } from 'react';
+import { RoundedBox, ScrollControls, Scroll, Environment, Sparkles, Backdrop, Float, Ring } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { Candies } from '../components/Candies';
 import { Header } from "../components/Header";
 
 const Historia = () => {
     return (
         <div>
             <Header />
-            <h1>Historia</h1>
+            <div className='historypage'>
+
+                <Canvas shadows>
+                    <color attach="background" args={['#fff']} />
+                    <ambientLight intensity={0.5} />
+                    <pointLight intensity={10} position={[5, 5, 5]} angle={1.3} penumbra={1} castShadow shadow-bias={-0.0001} />
+
+                    <Environment
+                        preset='warehouse'
+                    />
+
+                    <Candies />
+
+                    {/* <ScrollControls pages={6} damping={0.1}>
+                        <Scroll>
+                        </Scroll>
+
+                        <Scroll html style={{ width: '100%' }}>
+                            <h1 className='title' style={{ color: '#cdcbca', position: 'absolute', top: `65vh`, left: '50%', fontSize: '13em', transform: `translate(-50%,-50%)` }}>PHANTOM</h1>
+
+                            <div className='row' style={{ position: 'absolute', top: `132vh` }}>
+                                <div className='col' style={{ width: "540px" }}>
+                                    <h2>Be a Man of the Future.</h2>
+                                    <p style={{ maxWidth: '400px' }}>Featuring a sleek, metallic design inspired by advanced technology, this aftershave bottle is as stylish as it is functional. But it's not just a pretty face - inside, you'll find a nourishing and protective aftershave formula that will leave your skin feeling refreshed and hydrated.</p>
+                                    <button>Read more</button>
+                                </div>
+                            </div>
+
+                            <div className='row' style={{ position: 'absolute', top: `230vh` }}>
+                                <div className='col' style={{ position: 'absolute', right: `40px`, width: "540px" }}>
+                                    <h2 style={{ maxWidth: "440px" }}>Tech-Savvy Side</h2>
+                                    <p style={{ maxWidth: '440px' }}>Featuring a sleek, metallic design inspired by advanced technology, this aftershave bottle is as stylish as it is functional. But it's not just a pretty face - inside, you'll find a nourishing and protective aftershave formula that will leave your skin feeling refreshed and hydrated.</p>
+                                    <button>Read more</button>
+                                </div>
+                            </div>
+
+                            <h2 style={{ position: 'absolute', top: '350vh', left: '50%', transform: `translate(-50%,-50%)` }}>Cutting-Edge of Grooming</h2>
+
+                            <button style={{ position: 'absolute', top: `590vh`, left: '50%', transform: `translate(-50%,-50%)` }}>Buy now</button>
+                        </Scroll>
+                    </ScrollControls> */}
+                </Canvas>
+            </div>
         </div>
     );
 }
- 
+
 export default Historia;
